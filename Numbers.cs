05e73408cb5420db81768f8e6ad9f0323a4e6c24
agusmuhsin <muhammadagusmuhsin.tkj1@gmail.com>
2021-04-01 21:11:49 +0700

    Latihanoverloadingmethod

---
+++ Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverLoadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return (new int[] { number1, number2, }).Min();
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return (new int[] { number1, number2, }).Min();
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return (new int[] { number1, number2 }).Max();
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return (new int[] { number1, number2 }).Max();
+        }
+    }
+}
